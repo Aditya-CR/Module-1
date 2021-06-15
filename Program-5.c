f6eeaed5990ef21f261fb0390c86a686b731281b
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 11:30:13 +0530

    Create Program-5.c

--- Program-5.c
+++ Program-5.c
@@ -1,13 +1,13 @@
 //INPUT: 5 7
-//OUTPUT: 7 13   6 14
-// insert the missing line
+//OUTPUT: 7 13 6 14
+                       //Correct the code: insert the missing line
  int main ()
 { 
-     // insert the missing code
-printf("Enter the values of i and j");
+                      // Correct the code : Declare the variables
+//Enter the values of i and j
 scanf("%d%d",&i,&j);
      i = i + 1;
       j = j + i + 1  // correct the code
-      printf("%d %d   ",i + 1, j - 1);
-      printf("%d %d", i, j);
-return 0;}
+      printf("%d %d %d %d",i + 1, j - 1, i, j);
+return 0;
+ }
