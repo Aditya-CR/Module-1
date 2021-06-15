3d570d7ee116b474f8e9320a23b2c421119b625c
Aditya-CR <54580282+Aditya-CR@users.noreply.github.com>
2021-06-15 11:39:30 +0530

    Update Program-7.c

--- Program-7.c
+++ Program-7.c
@@ -1,18 +1,17 @@
 //INPUT: 95 90.99 A
-//OUTPUT: i value is: 90 x value is: 90.000000 i value is: 65
+//OUTPUT: 90.000000 65
 
 #include<stdio.h>
-int main() // correct the code
+int main() 
+              // correct the code
    int i;
    float x;
    char ch;
    printf("Enter an integer value, floating point value and a character");
    scanf("%d%f %",&i,&x,&ch);   // correct the code
-   i = x     // correct the code
-   printf("i value is: %d ",i);
+   i = x                         // correct the code
    x = i ;
-   printf("x value is: %f ",x);
    i = ch ;
-   printf("i value is: %d ",i);   
-return 0;
-// correct the code
+   printf("%f %d", x, i);
+   return 0;
+              // correct the code
